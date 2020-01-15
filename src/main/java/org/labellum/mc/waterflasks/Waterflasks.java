8f8d2c88a48c5d0bf2687e44bc8dff43a3429fea
Gaelmare <2gaelmare@gmail.com>
2020-01-14 23:21:32 -0700

    Bump version

--- src/main/java/org/labellum/mc/waterflasks/Waterflasks.java
+++ src/main/java/org/labellum/mc/waterflasks/Waterflasks.java
@@ -50,7 +50,7 @@ public class Waterflasks {
 
     public static final String MOD_ID = "waterflasks";
     public static final String MOD_NAME = "WaterFlasks";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 
     /**
      * Many thanks to Shadowfacts' 1.12.2 modding tutorial. Fingerprints from it remain...
